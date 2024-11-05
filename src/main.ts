9c6ef7617e9cadfa49fe8cf7d28fe6c071b7b827
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-11-04 21:35:44 -0800

    button

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 // todo
 const button: HTMLButtonElement = document.querySelector("#button")!;
-const app: HTMLDivElement = document.querySelector("#app")!;
-
 
 button.addEventListener("click", () => {
-    app.innerHTML = "YOUCLICKEDME";
+  alert("you clicked the button!");
 });
